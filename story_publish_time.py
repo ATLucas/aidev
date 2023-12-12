de1e16eb64e3f5b916030c4b1cf15a7de0bf5722
Andrew Lucas <andrew.tyler.lucas@gmail.com>
2023-12-12 17:10:29 -0500

    Update

--- story_publish_time.py
+++ story_publish_time.py
@@ -1,13 +1,16 @@
 from time import time
 import datetime
-future=int(input(''))/1000
-present=int(time())
-totsec=future-present
+
+future = int(1702296900000) / 1000
+present = int(time())
+totsec = future - present
 secs = str(int(totsec % 60))
 mins = str(int((totsec % 3600) / 60))
 hours = str(int((totsec % 86400) / 3600))
 days = str(int((totsec % (86400 * 30)) / 86400))
 futuredate = datetime.datetime.fromtimestamp(future)
 print("Your story is scheduled to be published on\n")
-print(str(futuredate)+" GMT. i.e.,\n")
-print("In "+days+" days "+ hours+" hours "+mins+" minutes "+secs+" seconds")
+print(str(futuredate) + " GMT. i.e.,\n")
+print(
+    "In " + days + " days " + hours + " hours " + mins + " minutes " + secs + " seconds"
+)
